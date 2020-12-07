330b7a0b553cae71418d67b78c5d23df8c696f4b
JoshBell302 <55896779+JoshBell302@users.noreply.github.com>
2020-12-07 01:23:52 -0800

    Created the partition_data function
    Completed the required function of partition_data

--- main.rs
+++ main.rs
@@ -1,4 +1,5 @@
 use std::env; // to get arugments passed to the program
+use std::thread;
 
 /*
 * Print the number of partitions and the size of each partition
@@ -120,11 +121,19 @@ fn main() {
     // MAP STEP: Process each partition
 
     // CHANGE CODE START: Don't change any code above this line
-
     // Change the following code to create 2 threads that run concurrently and each of which uses map_data() function to process one of the two partitions
 
-    intermediate_sums.push(map_data(&xs[0]));
-    intermediate_sums.push(map_data(&xs[1]));
+    let xs_imposter1 = xs[0].clone();
+    let xs_imposter2 = xs[1].clone();
+
+    let t1 = thread::spawn(move || map_data(&xs_imposter1));
+    let t2 = thread::spawn(move || map_data(&xs_imposter2));
+
+    let r1 = t1.join().unwrap();
+    let r2 = t2.join().unwrap();
+
+    intermediate_sums.push(r1);
+    intermediate_sums.push(r2);
 
     // CHANGE CODE END: Don't change any code below this line until the next CHANGE CODE comment
 
@@ -137,13 +146,40 @@ fn main() {
 
     // CHANGE CODE: Add code that does the following:
     // 1. Calls partition_data to partition the data into equal partitions
+    let xs2 = partition_data(num_partitions, &v);
     // 2. Calls print_partition_info to print info on the partitions that have been created
+    print_partition_info(&xs2);
     // 3. Creates one thread per partition and uses each thread to concurrently process one partition
-    // 4. Collects the intermediate sums from all the threads
-    // 5. Prints information about the intermediate sums
-    // 5. Calls reduce_data to process the intermediate sums
-    // 6. Prints the final sum computed by reduce_data
+    let mut intermediate_sums2 : Vec<usize> = Vec::new();
 
+    // Creating clone of data xs2
+  //  for a in 0..num_partitions {
+  //      let xs2_imposter = xs2[a].clone();
+   //     // Creating the threads 
+   //     for _b in 0..num_partitions {
+   //         let t = thread::spawn(move || map_data(&xs2_imposter));
+   //         // 4. Collects the intermediate sums from all the threads
+   //         for _c in 0..num_partitions {
+   //             let r = t.join().unwrap();
+   //             intermediate_sums2.push(r);
+    //        }
+   //     }
+    //}
+
+  // Testing the code
+  for i in 0..num_partitions {
+  let xs2_imposter = xs2[i].clone();
+  let t = thread::spawn(move || map_data(&xs2_imposter));
+  let r = t.join().unwrap();
+  intermediate_sums2.push(r);
+  }
+
+    // 5. Prints information about the intermediate sums
+    println!("Intermediate sums = {:?}", intermediate_sums2);
+    // 6. Calls reduce_data to process the intermediate sums
+    let sum2 = reduce_data(&intermediate_sums2);
+    // 7. Prints the final sum computed by reduce_data
+    println!("Sum = {}", sum2);
 }
 
 /*
@@ -161,6 +197,35 @@ fn main() {
 * 
 */
 fn partition_data(num_partitions: usize, v: &Vec<usize>) -> Vec<Vec<usize>>{
-    // Remove the following line which has been added to remove a compiler error
-    partition_data_in_two(v)
+   // Create a vector that will contain vectors of integers
+   let mut xs: Vec<Vec<usize>> = Vec::new();
+   let mut index = 0;
+
+   // Initialize Variables
+   let partition_size = v.len() / num_partitions;
+   let mut leftovers =  v.len() % num_partitions;
+   let mut leftover_done = 0;
+   // Make the vector that will hold the values  
+   for _i in 0..num_partitions {
+        let mut x1 : Vec<usize> = Vec::new();
+        // Pushing the values into x1 vector from v vector
+        for _j in 0..partition_size {
+            if index != v.len() { 
+            // If this is true then there are leftovers with the mod of parameters and it hasent been done for this vector yet
+                if leftovers > 0 && leftover_done == 0 {
+                    x1.push(v[index]);
+                    index = index + 1;
+                    leftovers = leftovers - 1;
+                    leftover_done = 1;
+                }
+            x1.push(v[index]);
+            index = index + 1;
+            }
+        }
+        // Push vector x1 onto xs and have leftovers_done back to 0 to check for a leftover vector
+        leftover_done = 0;
+        xs.push(x1);
+   }
+   // Return the result vector
+   xs
 }
